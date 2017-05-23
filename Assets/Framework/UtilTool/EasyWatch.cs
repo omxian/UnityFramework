948f7ce9eee807233ab356b4d71bfc406da6e4d2
omxian <104002782@qq.com>
2017-05-23 17:45:36 +0800

    简单易用的计时器

---
+++ Assets/Framework/UtilTool/EasyWatch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+/// <summary>
+/// 简单计时器
+/// 使用方式 using (new EasyWatch("test1",1000))　{//code}
+/// </summary>
+public class EasyWatch : IDisposable
+{
+    private string name;
+    private int count;
+    private Stopwatch watch;
+
+    public EasyWatch(string name, int count)
+    {
+        this.name = name;
+        this.count = count;
+        watch = Stopwatch.StartNew();
+    }
+
+    public void Dispose()
+    {
+        watch.Stop();
+        UnityEngine.Debug.Log(string.Format("{0} 总耗时:{1} 平均耗时:{2} 运行次数{3}", 
+            name, 
+            watch.ElapsedMilliseconds, 
+            watch.ElapsedMilliseconds / count,
+            count
+            ));
+    }
+}
