9bc52409c12bebb9ac03c22cfcd57a943fb2ffa5
omxian <104002782@qq.com>
2017-09-08 18:39:44 +0800

    Raw Image UV滚动(羊皮卷效果)

---
+++ Assets/Script/Effect/RawImageUV.cs
@@ -0,0 +1,39 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RawImageUV : MonoBehaviour
+{
+    public RawImage target;
+
+    public float SpeedX = 0f;
+    public float SpeedY = 0f;
+
+    private float ox;
+    private float oy;
+    private float wc;
+    private float hc;
+
+    private void Start()
+    {
+        if (target == null)
+        {
+            target = GetComponent<RawImage>();
+        }
+
+        wc = target.rectTransform.rect.width / target.mainTexture.width;
+        hc = target.rectTransform.rect.height / target.mainTexture.height;
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            ox += Time.deltaTime * SpeedX;
+            oy += Time.deltaTime * SpeedY;
+            ox = ox % 1;
+            oy = oy % 1;
+            target.GetComponent<RawImage>().uvRect = new Rect(ox, oy, wc, hc);
+        }
+    }
+}
