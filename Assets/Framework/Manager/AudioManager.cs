78032dcca53d717903f2c24142ba5354ee3c83ee
omxian <104002782@qq.com>
2017-09-18 23:55:33 +0800

    TODO AudioManager

---
+++ Assets/Framework/Manager/AudioManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoSingleton<AudioManager>
+{
+
+    private AudioManager()
+    {
+    }
+
+    public override void Init()
+    {
+
+    }
+}
--- Assets/Framework/Manager/SceneManager.cs
+++ Assets/Framework/Manager/SceneManager.cs
@@ -1,12 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
-//SceneManager 的命名和系统的命名冲突需要修改
-namespace Framework.Owen
+public class SceneManager : MonoSingleton<SceneManager>
 {
-    public class SceneManager : MonoSingleton<SceneManager>
+    private SceneManager()
     {
-        private SceneManager()
-        {
-        }    
-    }
+    }    
 }
