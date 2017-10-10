a7c48d77b95de78d71ff4cb1587fd409bdcef6d8
Owen <104002782@qq.com>
2017-10-10 10:25:33 +0800

    ImageUV新做法，图片置灰解决方案

---
+++ Assets/Solution/ImageEffect/Editor/ImageUVInspector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ImageUV_New))]
+public class ImageUVInspector : Editor {
+    public override void OnInspectorGUI()
+    {
+        //必须调用才能显示出序列化的属性
+        //否则不会显示出来，因为Image组件内部做了处理
+        base.OnInspectorGUI();
+    }
+}
---
+++ Assets/Solution/ImageEffect/ImageUV_New.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ImageUV_New : Image
+{
+    //不知道为什么无法序列化出去，需要写个Inspector
+    public Vector2 v2 = new Vector2(2f,2f);
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector2 to = Time.deltaTime * v2;
+        //to.x %= 1.0f;
+        //to.y %= 1.0f;
+        //material.mainTextureOffset += to;
+
+        Vector2 to = Time.time * v2;
+        to.x %= 1.0f;
+        to.y %= 1.0f;
+        material.SetTextureOffset("_MainTex", to);
+    }
+}
