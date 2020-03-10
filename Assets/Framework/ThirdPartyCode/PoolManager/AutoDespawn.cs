e262fa1e49c932262d25e96ddacb652a084f83c0
omxian <104002782@qq.com>
2020-03-10 14:51:18 +0800

    新增对象池代码，以及一个应用的例子

---
+++ Assets/Framework/ThirdPartyCode/PoolManager/AutoDespawn.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/// <summary>
+/// 设计用于配合使用PoolManager使用
+/// 在若干秒之后返回对象到对象池
+/// 实现自动回收特效
+/// </summary>
+public class AutoDespawn : MonoBehaviour
+{
+    public string poolName = "GamePool";
+    public float time = 2.0f;
+    private SpawnPool pool = null;
+    private bool beSpawned = true;
+
+    void Awake()
+    {
+        PoolManager.Pools.TryGetValue(poolName, out pool);
+    }
+
+
+    void Start()
+    {
+        if (time > 0) Invoke("DeSpawnEffect", time);
+    }
+
+
+    void OnSpawned()
+    {
+        beSpawned = true;
+
+        if (!pool)
+        {
+            PoolManager.Pools.TryGetValue(poolName, out pool);
+        }
+
+        if (time > 0)
+        {
+            Invoke("DeSpawnEffect", time);
+        }
+    }
+
+
+    void OnDespawned()
+    {
+        if (!beSpawned)
+        {
+            return;
+        }
+
+        beSpawned = false;
+        gameObject.SetActive(false);
+
+    }
+
+    private void DeSpawnEffect()
+    {
+        StopAllCoroutines();
+        if (pool == null)
+        {
+            Debug.LogWarningFormat("{0} is null", poolName);
+            return;
+        }
+        pool.Despawn(transform);
+    }
+}
---
+++ Assets/Solution/PoolManager/PoolManagerTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManagerTest : MonoBehaviour
+{
+    public Transform pos1;
+    public Transform pos2;
+    private Transform currPos;
+    public Transform effect;
+    public float interval;
+    private float currentTime;
+    private SpawnPool pool;
+    private Transform GetPosition()
+    { 
+        if(currPos== null)
+        {
+            currPos = pos1;
+        }
+        else if (currPos == pos1)
+        {
+            currPos = pos2;
+        }
+        else
+        {
+            currPos = pos1;
+        }
+        return currPos;
+    }
+
+
+    void Start()
+    {
+        pool = PoolManager.Pools.Create("Pet");
+    }
+    private void PlayEffect()
+    {
+        pool.Spawn(effect).SetParent(GetPosition(), false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if(currentTime > interval)
+        {
+            currentTime = 0;
+            PlayEffect();
+        }
+    }
+}
