6a8f2838811c1879d70cf47dcec0e6aa2d44bbc3
owen <104002782@qq.com>
2020-12-24 17:14:58 +0800

    新手引导类型遮罩1

---
+++ Assets/Solution/GuideMask1/GuidanceEventPenetrate.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class GuidanceEventPenetrate : MonoBehaviour, ICanvasRaycastFilter
+{
+	private RectTransform mTagrget;
+
+	public void SetTarget(RectTransform target)
+	{
+		mTagrget = target;
+	}
+	public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
+	{
+		if (mTagrget == null)
+			return true;
+
+		return !RectTransformUtility.RectangleContainsScreenPoint(mTagrget, sp, eventCamera);
+	}
+}
---
+++ Assets/Solution/GuideMask1/TestMask.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestMask : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button target;
+    public Button target2;
+    void Start()
+    {
+        target.onClick.AddListener(() => {
+            Debug.Log("AAA Click");
+        });
+
+        target.onClick.AddListener(() => {
+            Debug.Log("BBB Click");
+        });
+
+        StartCoroutine(NextMove());
+    }
+
+    private IEnumerator NextMove()
+    {
+        GetComponent<RectGuidanceController>().SetTarget(target.transform as RectTransform, false);
+        yield return new WaitForSeconds(2);
+        GetComponent<RectGuidanceController>().SetTarget(target2.transform as RectTransform, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
